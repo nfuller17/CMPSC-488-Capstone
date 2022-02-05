ee770ef8f858967a9421d9dd9712666dd83239bb
Kenny <kennynhim@gmail.com>
2022-02-05 18:26:21 -0500

    create AI controller

---
+++ Source/TomAndJerry/Public/ControllerTom.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ControllerTom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMANDJERRY_API AControllerTom : public AAIController
+{
+	GENERATED_BODY()
+	
+};
