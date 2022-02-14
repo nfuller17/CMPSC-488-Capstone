6c1704e49680378ad6d9d0ec88ae79a77d3fcd4b
Kenny <kennynhim@gmail.com>
2022-02-13 19:42:09 -0500

    create custom player controller to handle material inventory

---
+++ Source/TomAndJerry/Public/ControllerJerry.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ControllerJerry.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMANDJERRY_API AControllerJerry : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
