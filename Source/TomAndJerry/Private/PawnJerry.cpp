feadbfe93757903e1e50e94c26ccb13d0e9da8c6
Kenny <kennynhim@gmail.com>
2022-02-27 23:01:52 -0500

    have player interact with minor objective

--- Source/TomAndJerry/Private/PawnJerry.cpp
+++ Source/TomAndJerry/Private/PawnJerry.cpp
@@ -4,6 +4,7 @@
 #include "PawnJerry.h"
 #include "WeaponMaterial.h"
 #include "ControllerJerry.h"
+#include "MinorObjective.h"
 
 // Sets default values
 APawnJerry::APawnJerry()
@@ -48,18 +49,35 @@ void APawnJerry::Strafe(float AxisValue)
 	AddMovementInput(GetActorRightVector() * AxisValue);
 }
 
-//Called when player walks over a weapon material
+//Called when player walks over a weapon material or minor objective
 //Calls the AddWeaponMaterial in ControllerJerry, and destroys the material
+//Or activates the Minor Objective
 void APawnJerry::NotifyActorBeginOverlap(AActor* OtherActor)
 {
 	Super::NotifyActorBeginOverlap(OtherActor);
 	AWeaponMaterial* WeaponMaterial = Cast<AWeaponMaterial>(OtherActor);
 	if (WeaponMaterial) {
-		UE_LOG(LogTemp, Warning, TEXT("Player walked over a Weapon Material!"));
+		//UE_LOG(LogTemp, Warning, TEXT("Player walked over a Weapon Material!"));
 		AControllerJerry* playerController = Cast<AControllerJerry>(GetController());
 		if (playerController) {
 			playerController->AddWeaponMaterial(WeaponMaterial->GetMaterialID());
 			WeaponMaterial->Destroy();
 		}
+	}
+	else {	//Not a Weapon Material. Check if we are standing on a Minor Objective
+		AMinorObjective* MinorObjective = Cast<AMinorObjective>(OtherActor);
+		if (MinorObjective) {
+			MinorObjective->BeginActivation();
+		}
+	}
+}
+
+//Called when player walks off a Minor Objective
+void APawnJerry::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+	AMinorObjective* MinorObjective = Cast<AMinorObjective>(OtherActor);
+	if (MinorObjective) {
+		MinorObjective->StopActivation();
 	}
 }
---
+++ Source/TomAndJerry/Public/MinorObjective.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MinorObjective.generated.h"
+
+UCLASS()
+class TOMANDJERRY_API AMinorObjective : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMinorObjective();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called when player stands on Minor Objective
+	void BeginActivation();
+	// Called when player walks off Minor Objective
+	void StopActivation();
+	// Called when player successfully completes Minor Objective
+	void Complete();
+	FTimerHandle ActivationTimer;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float CompletionTime = 10.0;	//How long, in seconds, the player must stand on a Minor Objective to complete it
+
+};
